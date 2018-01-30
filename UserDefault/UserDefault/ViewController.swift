51a939195eba681a31723fb2b8f18576a8034942
luiscastillo27 <luisyosemite@gmail.com>
2018-01-30 14:24:25 -0600

    final de la app
    se incorporaron alertas

--- UserDefault/UserDefault/ViewController.swift
+++ UserDefault/UserDefault/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var txtResult: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +20,29 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func btnGuardar(_ sender: UIButton) {
+        if( txtResult.text == ""){
+            let alerta = UIAlertController(title: "Alerta", message: "Escribe algo", preferredStyle: .alert);
+            let aceptar = UIAlertAction(title: "Jim Fea", style: .default, handler: nil);
+            alerta.addAction(aceptar);
+            present(alerta, animated: true, completion: nil);
+        } else {
+            UserDefaults.standard.set(txtResult.text, forKey: "Jim");
+            txtResult.text = "";
+            txtResult.resignFirstResponder();
+        }
+    }
+    
+    @IBAction func btnMostrar(_ sender: UIButton) {
+        let result = UserDefaults.standard.object(forKey: "Jim");
+        txtResult.text = result as? String;
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true);
+    }
+    
+    
 }
 
