d412319bf84dc127bb7ca06178de6a43de4b3310
luiscastillo27 <luisyosemite@gmail.com>
2018-02-06 17:09:26 -0600

    iniciando proyecto

---
+++ firestore/firestore/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  firestore
+//
+//  Created by Luis on 06/02/18.
+//  Copyright © 2018 Luis. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var txtNombre: UITextField!
+    @IBOutlet weak var txtApellido: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func btnGuardar(_ sender: UIButton) {
+        
+    }
+    
+
+
+}
+
