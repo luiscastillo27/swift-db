158d0583c80af9c66cae0b055df5ba6a90bdd1d2
Luis Castillo Z <luiscastillo@iwebsapp.com>
2018-03-01 11:11:32 -0600

    paginacion view

---
+++ pagesView/pagesView/PrimeroController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pagesView
+//
+//  Created by luis on 01/03/18.
+//  Copyright © 2018 luis. All rights reserved.
+//
+
+import UIKit
+
+class PrimeroController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
