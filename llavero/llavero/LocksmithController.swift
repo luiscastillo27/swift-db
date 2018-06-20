28d2f698b1645693dacf60efd8a078e54a512536
Luis Castillo <luiscastillo@iwebsapp.com>
2018-06-20 17:56:05 -0500

    Locksmith

--- llavero/llavero/LocksmithController.swift
+++ llavero/llavero/LocksmithController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Locksmith
 
 class LocksmithController: UIViewController {
 
@@ -16,15 +17,36 @@ class LocksmithController: UIViewController {
     }
 
     @IBAction func btnSave(_ sender: UIButton) {
-        
+        do {
+            try Locksmith.saveData(data: ["myKey27": txtValue.text!, "myKey15": "Que onda"], forUserAccount: "jimcastillo")
+            print("Se ha guardado correctamente")
+        } catch let error as NSError {
+            print("No se ha guardado: \(error)")
+        }
     }
     
     @IBAction func btnShow(_ sender: UIButton) {
-        
+        let show = Locksmith.loadDataForUserAccount(userAccount: "jimcastillo")
+        print("mis llaves son:", show?["myKey27"] ?? "Nada que mostrar")
     }
     
+    @IBAction func btnUpdate(_ sender: UIButton) {
+        do {
+            try Locksmith.updateData(data: ["myKey27": txtValue.text!], forUserAccount: "jimcastillo")
+            print("Se actualizo correctamente")
+        } catch let error as NSError {
+            print("Se ha producido un error: \(error)")
+        }
+    }
+    
+    
     @IBAction func btnDelete(_ sender: UIButton) {
-        
+        do {
+            try Locksmith.deleteDataForUserAccount(userAccount: "jimcastillo")
+            print("se ha eliminado correctamente")
+        } catch let error as NSError {
+            print("se ha producido un error: \(error)")
+        }
     }
     
     
